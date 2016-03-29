3911dcd76eae4b44f0574ebae551b6b395c13a53
Tobschinski <tobschinski@gmail.com>
2016-03-29 15:57:53 +0200

    Hellfire Ramparts - missing equipment

---
+++ Updates/Custom_Data/0016_01_creature_template.sql
@@ -0,0 +1,9 @@
+
+-- Bleeding Hollow Scryer
+UPDATE `creature_template` SET `EquipmentTemplateId`='5598' WHERE `Entry`='17478';
+
+-- Bonechewer Beastmaster
+UPDATE `creature_template` SET `EquipmentTemplateId`='5265' WHERE `Entry`='17455';
+
+-- Hellfire Sentry
+UPDATE `creature_template` SET `EquipmentTemplateId`='5597' WHERE `Entry`='17517';
