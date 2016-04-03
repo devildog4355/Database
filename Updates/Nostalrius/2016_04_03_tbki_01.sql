6b5c7c3b342e20a61fe709dc79ce5d1ee36276e9
Tobschinski <tobschinski@gmail.com>
2016-04-03 04:17:58 +0200

    The Shattered Halls - Equipment
    * added equipment to creatures in the Shattered Halls
    * added equipment to Targar Spinebreaker

---
+++ Updates/Nostalrius/2016_04_03_tbki_01.sql
@@ -0,0 +1,85 @@
+
+DELETE FROM `item_template` WHERE `entry` IN ('18419', '21554', '12902', '13160', '22199', '29484');
+INSERT INTO `item_template` (`entry`, `class`, `subclass`, `unk0`, `name`, `displayid`, `Quality`, `Flags`, `BuyCount`, `BuyPrice`, `SellPrice`, `InventoryType`, `AllowableClass`, `AllowableRace`, `ItemLevel`, `RequiredLevel`, `RequiredSkill`, `RequiredSkillRank`, `requiredspell`, `requiredhonorrank`, `RequiredCityRank`, `RequiredReputationFaction`, `RequiredReputationRank`, `maxcount`, `stackable`, `ContainerSlots`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `dmg_min1`, `dmg_max1`, `dmg_type1`, `dmg_min2`, `dmg_max2`, `dmg_type2`, `dmg_min3`, `dmg_max3`, `dmg_type3`, `dmg_min4`, `dmg_max4`, `dmg_type4`, `dmg_min5`, `dmg_max5`, `dmg_type5`, `armor`, `holy_res`, `fire_res`, `nature_res`, `frost_res`, `shadow_res`, `arcane_res`, `delay`, `ammo_type`, `RangedModRange`, `spellid_1`, `spelltrigger_1`, `spellcharges_1`, `spellppmRate_1`, `spellcooldown_1`, `spellcategory_1`, `spellcategorycooldown_1`, `spellid_2`, `spelltrigger_2`, `spellcharges_2`, `spellppmRate_2`, `spellcooldown_2`, `spellcategory_2`, `spellcategorycooldown_2`, `spellid_3`, `spelltrigger_3`, `spellcharges_3`, `spellppmRate_3`, `spellcooldown_3`, `spellcategory_3`, `spellcategorycooldown_3`, `spellid_4`, `spelltrigger_4`, `spellcharges_4`, `spellppmRate_4`, `spellcooldown_4`, `spellcategory_4`, `spellcategorycooldown_4`, `spellid_5`, `spelltrigger_5`, `spellcharges_5`, `spellppmRate_5`, `spellcooldown_5`, `spellcategory_5`, `spellcategorycooldown_5`, `bonding`, `description`, `PageText`, `LanguageID`, `PageMaterial`, `startquest`, `lockid`, `Material`, `sheath`, `RandomProperty`, `RandomSuffix`, `block`, `itemset`, `MaxDurability`, `area`, `Map`, `BagFamily`, `TotemCategory`, `socketColor_1`, `socketContent_1`, `socketColor_2`, `socketContent_2`, `socketColor_3`, `socketContent_3`, `socketBonus`, `GemProperties`, `RequiredDisenchantSkill`, `ArmorDamageModifier`, `ScriptName`, `DisenchantID`, `FoodType`, `minMoneyLoot`, `maxMoneyLoot`, `Duration`, `ExtraFlags`) VALUES
+('18419', '2', '1', '-1', 'Monster - Axe, 2H Horde Red War Axe' , '28207', '0', '0', '1', '0', '0', '17', '-1', '-1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '3400', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0'),
+('21554', '2', '3', '-1', 'Monster - Gun, PvP Horde' , '31747', '0', '0', '1', '0', '0', '26', '-1', '-1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '3400', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0'),
+('12902', '2', '8', '-1', 'Monster - Sword2H, Luminous Evil Blade' , '23405', '0', '0', '1', '0', '0', '17', '-1', '-1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '3400', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0'),
+('13160', '2', '8', '-1', 'Monster - Sword2H, Claymore Silver' , '20195', '0', '0', '1', '0', '0', '17', '-1', '-1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '3400', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0'),
+('22199', '2', '1', '-1', 'Monster - Axe, 2H Arcanite Reaper' , '23904	', '0', '0', '1', '0', '0', '17', '-1', '-1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '3400', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0'),
+('29484', '2', '5', '-1', 'Monster - Mace2H, Firemaul of Destruction (No Fire)' , '39278', '0', '0', '1', '0', '0', '17', '-1', '-1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '3400', '0', '0', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '-1', '0', '', '0', '0', '0', '0', '0', '0');
+
+DELETE FROM `creature_equip_template` WHERE `entry` BETWEEN '5599' AND '5615';
+INSERT INTO `creature_equip_template` (`entry`, `equipentry1`, `equipentry2`, `equipentry3`) VALUES
+('5599', '13707', '13707', '0'), -- Shattered Hand Heathen
+('5600', '18419', '0', '21554'), -- Shattered Hand Reaver / Shattered Hand Executioner
+('5601', '0', '12893', '0'),     -- Shattered Hand Sentry
+('5602', '10619', '2711', '0'),  -- Shattered Hand Assassin
+('5603', '0', '0', '21554'),     -- Shattered Hand Sharpshooter
+('5604', '12290', '12290', '0'), -- Shattered Hand Savage
+('5605', '2147', '0', '23052'),  -- Shattered Hand Houndmaster
+('5606', '12902', '0', '0'),     -- Grand Warlock Nethekurse
+('5607', '13160', '0', '0'),     -- Blood Guard Porung
+('5608', '11019', '0', '0'),     -- Shattered Hand Zealot
+('5609', '2147', '0', '14118'),  -- Shattered Hand Archer
+('5610', '1906', '0', '0'),      -- Shattered Hand Scout
+('5611', '14870', '0', '21554'), -- Shattered Hand Gladiator
+('5612', '22199', '0', '0'),     -- Shattered Hand Centurion
+('5613', '10615', '12995', '0'), -- Shattered Hand Champion
+('5614', '29484', '0', '0'),     -- Warbringer O'mrogg
+-- ('x', '0', '2081', '0'),   -- Shattered Hand Legionnaire | -> 5045
+('5615', '22199', '0', '0'); -- Tagar Spinebreaker
+
+-- Tagar Spinebreaker
+UPDATE `creature_template` SET `EquipmentTemplateId`='5615' WHERE `Entry`='19443';
+
+-- Shattered Hand Heathen
+UPDATE `creature_template` SET `EquipmentTemplateId`='5599' WHERE `Entry`='17420';
+
+-- Shattered Hand Reaver / Shattered Hand Executioner
+UPDATE `creature_template` SET `EquipmentTemplateId`='5600' WHERE `Entry` IN ('16699', '17301');
+
+-- Shattered Hand Sentry
+UPDATE `creature_template` SET `EquipmentTemplateId`='5601' WHERE `Entry`='16507';
+
+-- Shattered Hand Assassin
+UPDATE `creature_template` SET `EquipmentTemplateId`='5602' WHERE `Entry`='17695';
+
+-- Shattered Hand Sharpshooter
+UPDATE `creature_template` SET `EquipmentTemplateId`='5603' WHERE `Entry`='16704';
+
+-- Shattered Hand Savage
+UPDATE `creature_template` SET `EquipmentTemplateId`='5604' WHERE `Entry`='16523';
+
+-- Shattered Hand Houndmaster
+UPDATE `creature_template` SET `EquipmentTemplateId`='5605' WHERE `Entry`='17670';
+
+-- Grand Warlock Nethekurse
+UPDATE `creature_template` SET `EquipmentTemplateId`='5606' WHERE `Entry`='16807';
+
+-- Blood Guard Porung / Shattered Hand Blood Guard
+UPDATE `creature_template` SET `EquipmentTemplateId`='5607' WHERE `Entry` IN ('20923', '17461');
+
+-- Shattered Hand Zealot
+UPDATE `creature_template` SET `EquipmentTemplateId`='5608' WHERE `Entry`='17462';
+
+-- Shattered Hand Archer
+UPDATE `creature_template` SET `EquipmentTemplateId`='5609' WHERE `Entry`='17427';
+
+-- Shattered Hand Scout
+UPDATE `creature_template` SET `EquipmentTemplateId`='5610' WHERE `Entry`='17693';
+
+-- Shattered Hand Gladiator
+UPDATE `creature_template` SET `EquipmentTemplateId`='5611' WHERE `Entry`='17464';
+
+-- Shattered Hand Centurion
+UPDATE `creature_template` SET `EquipmentTemplateId`='5612' WHERE `Entry`='17465';
+
+-- Shattered Hand Champion
+UPDATE `creature_template` SET `EquipmentTemplateId`='5613' WHERE `Entry`='17671';
+
+-- Warbringer O'mrogg
+UPDATE `creature_template` SET `EquipmentTemplateId`='5614' WHERE `Entry`='16809';
+UPDATE `creature_template` SET `EquipmentTemplateId`='0'    WHERE `Entry`='20596';
+
+-- Shattered Hand Legionnaire
+UPDATE `creature_template` SET `EquipmentTemplateId`='5045' WHERE `Entry`='16700';
